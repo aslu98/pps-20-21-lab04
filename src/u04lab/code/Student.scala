99ed29627f725eaaada55d2c061454a073b4671a
Asia <asia.lucchi@studio.unibo.it>
2021-03-23 11:28:55 +0100

    SameTeacher completed

--- src/u04lab/code/Student.scala
+++ src/u04lab/code/Student.scala
@@ -1,44 +1,71 @@
 package u04lab.code
 
 import Lists._
-import u04lab.code.Lists.List.{Cons, appendByFold, length} // import custom List type (not the one in Scala stdlib)
+import u04lab.code.Lists.List.{Cons, appendByFold, length}
+import u04lab.code.Try.courses // import custom List type (not the one in Scala stdlib)
 
 
-trait Student {
-  def name: String
-  def year: Int
-  def enrolling(courses: Course*): Unit // the student participates to a Course
-  def courses: List[String] // names of course the student participates to
-  def hasTeacher(teacher: String): Boolean // is the student participating to a course of this teacher?
-}
+object University {
 
-trait Course {
-  def name: String
-  def teacher: String
-}
+  trait Student {
+    def name: String
 
-object Student {
-  def apply(name: String, year: Int = 2017): Student = StudentImpl(name, year)
+    def year: Int
 
-  private case class StudentImpl(override val name:String, override  val year:Int) extends Student {
-    private var coursesSet: List[Course] = List.nil
-    override def enrolling(courses: Course*): Unit = for (course <- courses) {
-      coursesSet = Cons(course, coursesSet)
-    }
-    override def courses: List[String] = List.map(coursesSet)(_.name)
-    override def hasTeacher(teacher: String): Boolean = length(List.filter(coursesSet)(_.teacher == teacher)) >= 1
+    def enrolling(courses: Course*): Unit // the student participates to a Course
+    def courses: List[String] // names of course the student participates to
+    def hasTeacher(teacher: String): Boolean // is the student participating to a course of this teacher?
   }
-}
 
-object Course {
-  def apply(name: String, teacher: String): Course = CourseImpl(name, teacher)
+  trait Course {
+    def name: String
 
-  private case class CourseImpl(override val name:String, override  val teacher:String) extends Course {
-   assert(name!= null && teacher !=null)
+    def teacher: String
+  }
+
+  object Student {
+    def apply(name: String, year: Int = 2017): Student = StudentImpl(name, year)
+
+    private case class StudentImpl(override val name: String, override val year: Int) extends Student {
+      private var coursesSet: List[Course] = List.nil
+
+      override def enrolling(courses: Course*): Unit = for (course <- courses) {
+        coursesSet = Cons(course, coursesSet)
+      }
+
+      override def courses: List[String] = List.map(coursesSet)(_.name)
+
+      override def hasTeacher(teacher: String): Boolean = length(List.filter(coursesSet)(_.teacher == teacher)) >= 1
+    }
+
+  }
+
+  object Course {
+    def apply(name: String, teacher: String): Course = CourseImpl(name, teacher)
+
+    private case class CourseImpl(override val name: String, override val teacher: String) extends Course {
+      assert(name != null && teacher != null)
+    }
+  }
+
+  object sameTeacher {
+    def unapply(courses: List[Course]): Option[String] = {
+      val firstTeacher: String = courses match {
+        case Cons(h, _) => h.teacher
+      }
+      Option.when(List.foldLeft(courses)(true)((b, c) => b && c.teacher == firstTeacher))(firstTeacher)
+    }
+  }
+
+  def printSameTeacher(courses: List[Course]): Unit = courses match {
+    case sameTeacher(t) => println(s" $courses have same teacher $t")
+    case _ => println(s" $courses have different teachers ")
   }
 }
 
 object Try extends App {
+  import University._
+
   val cPPS = Course("PPS","Viroli")
   val cPCD = Course("PCD","Ricci")
   val cSDR = Course("SDR","D'Angelo")
@@ -52,6 +79,16 @@ object Try extends App {
   s3.enrolling(cSDR)
   println(s1.courses, s2.courses, s3.courses) // (Cons(PCD,Cons(PPS,Nil())),Cons(PPS,Nil()),Cons(SDR,Cons(PCD,Cons(PPS,Nil()))))
   println(s1.hasTeacher("Ricci")) // true
+
+  val cOOP = Course("PCD","Viroli")
+  val courses = List(cPPS, cOOP)
+  printSameTeacher(courses)
+
+  val coursesEmpty = List(cPPS)
+  printSameTeacher(coursesEmpty)
+
+  val coursesDifferent = List(cPPS, cSDR)
+  printSameTeacher(coursesDifferent)
 }
 
 /** Hints:
